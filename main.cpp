9a3b2856a7b7a1edd833e91a6fc4916fafd04272
Paul Scarrone <ninjapanzer@gmail.com>
2015-11-05 21:12:24 -0500

    adding resources so this program runs and displays a bitmap

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include "res_path.h"
 
 using namespace std;
-
-SDL_Event event;
 
 int main(int argc, char *argv[]){
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
@@ -11,15 +10,57 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    bool done=false;
-    while(!done) {
-        while(SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                done=true;
-            }
-        }
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr){
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
     }
 
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr){
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    std::string imagePath = getResourcePath("") + "fun.bmp";
+    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+    if (bmp == nullptr){
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+    SDL_FreeSurface(bmp);
+    if (tex == nullptr){
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    for (int i = 0; i < 3; ++i){
+        //First clear the renderer
+        SDL_RenderClear(ren);
+        //Draw the texture
+        SDL_RenderCopy(ren, tex, NULL, NULL);
+        //Update the screen
+        SDL_RenderPresent(ren);
+        //Take a quick break after all that hard work
+        SDL_Delay(1000);
+    }
+
+
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
     SDL_Quit();
 
     return 0;
